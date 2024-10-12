5eb09a106d40d4b8b6aa81b3653b923a91a84b03
andrew <andrew.kourilenko@netafim.com>
2024-10-12 10:02:22 +0300

    first commit

--- database.py
+++ database.py
@@ -10,4 +10,4 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-base = declarative_base()
+Base = declarative_base()
