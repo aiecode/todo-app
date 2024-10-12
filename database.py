f002029e1b0ae2a18ef674877627119b00a6483b
andrew <andrew.kourilenko@netafim.com>
2024-10-12 21:34:12 +0300

    first commit

--- database.py
+++ database.py
@@ -7,7 +7,6 @@ SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
-session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
